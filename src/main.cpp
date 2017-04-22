4adbec768f3e5abc5823aa0b3c85e0a661172d0f
Noobidoo <ttraskback@gmail.com>
2017-04-22 23:21:09 +0300

    Add rest of setup commands
    These are just copys from
    demo provided needs to be adjusted
    for our setup still

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,57 @@ inline void sendData(uint8_t data)
     TFT_CS_HIGH;
 }
 
+void pic_display (void)
+{
+  unsigned long int i;
+	unsigned char j,temp1,temp2,temp3;
+
+
+	sendCMD(0x10);	       //Start picture update
+
+	for(i=0;i<61440;i++)
+	{
+		temp1 = 0XFF;
+		for(j=0;j<4;j++)
+		{
+			temp2 = temp1&0xc0 ;
+			if(temp2 == 0xc0)
+				temp3 = 0x00;
+			else if(temp2 == 0x00)
+				temp3 = 0x03;
+			else
+				temp3 = 0x04;
+
+			temp1 <<= 2;
+			temp3 <<= 4;
+			j++;
+
+			temp2 = temp1&0xc0 ;
+			if(temp2 == 0xc0)
+				temp3 |= 0x00;
+			else if(temp2 == 0x00)
+				temp3 |= 0x03;
+			else
+				temp3 |= 0x04;
+
+			temp1 <<= 2;
+
+			sendData(temp3);
+		}
+	}
+}
+void waitForRedy(){
+  unsigned char busy;
+  do
+  {
+    sendCMD(0x71);
+    busy = TFT_BUSY;
+    busy =!(busy & 0x01);
+  }
+  while(busy);
+  delay(200);
+}
+
 void setup()
 {
     initDisplay();                                      //init display
@@ -33,6 +84,7 @@ void initDisplay()
   sendCMD  (0x01);
   sendData (0x37);
   sendData (0x00);
+  
   //PANNEL SETTING
   sendCMD  (0x00);
   sendData (0xCF);
@@ -56,21 +108,39 @@ void initDisplay()
   sendCMD(0X50);
   sendData(0x77);
 
-  sendCMD(0X60);     //TCON SETTING
+  //TCON SETTING
+  sendCMD(0X60);
   sendData(0x22);
 
-  sendCMD(0x61);         //tres
+  //Resolution
+  sendCMD(0x61);
   sendData (0x02);   //source 640
   sendData (0x80);
   sendData (0x01);   //gate 384
   sendData (0x80);
 
-
-  sendCMD(0X82);     //VDCS SETTING
+  //VDCS SETTING
+  sendCMD(0X82);
   sendData(0x1E);    //decide by LUT file
 
-  sendCMD(0xe5);     //FLASH MODE
+  //FLASH MODE
+  sendCMD(0xe5);
   sendData(0x03);
+
+  //pic_display();
+  //POWER ON
+  sendCMD(0x04);
+  waitForRedy();
+
+  //display refresh
+  sendCMD(0x12);
+  waitForRedy();
+
+  sendCMD(0x02);
+  waitForRedy();
+
+  sendCMD(0x07);
+  sendData(0xa5);
 }
 
 void loop()
