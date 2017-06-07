21147f907810838562897dfd06339754b9729767
Noobidoo <ttraskback@gmail.com>
2017-06-08 01:04:45 +0300

    Added flash release commmand

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,23 @@ void initDisplay();
 
 inline void sendCMD(uint8_t index)
 {
-    TFT_DC_LOW;
     TFT_CS_LOW;
+    TFT_DC_LOW;
     SPI.transfer(index);
     TFT_CS_HIGH;
 }
 
 inline void sendData(uint8_t data)
 {
-    TFT_DC_HIGH;
     TFT_CS_LOW;
+    TFT_DC_HIGH;
     SPI.transfer(data);
     TFT_CS_HIGH;
+}
+
+void MCU_write_flash(uint8_t command)
+{
+  SPI.transfer(command);
 }
 
 void pic_display (unsigned char write)
@@ -33,6 +38,7 @@ void pic_display (unsigned char write)
 	for(i=0;i<61440;i++)
 	{
 		temp1 = 0XFF;
+    sendData(write);
 		for(j=0;j<4;j++)
 		{
 			temp2 = temp1&0xc0 ;
@@ -57,7 +63,7 @@ void pic_display (unsigned char write)
 
 			temp1 <<= 2;
 
-			sendData(0xff);
+
 			//sendData(temp3);
 		}
 	}
@@ -66,7 +72,7 @@ void waitForRedy(){
   unsigned char busy;
   do
   {
-    sendCMD(0x71);
+    //sendCMD(0x71);
     busy = TFT_BUSY;
     busy =! busy;
   }
@@ -79,7 +85,7 @@ void setupPins(){
   pinMode(TFT_CS_PIN,OUTPUT);
   pinMode(TFT_DC_PIN,OUTPUT);
   pinMode(TFT_RST_PIN,OUTPUT);
-  pinMode(TFT_BUSY_PIN,INPUT_PULLUP);
+  pinMode(TFT_BUSY_PIN,INPUT);
 }
 void setup()
 {
@@ -90,7 +96,7 @@ void setup()
     Serial.println("Setup");
     setupPins();
     SPI.begin();
-    SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
+    SPI.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE0));
 
     initDisplay();                                      //init display
     SPI.endTransaction();
@@ -105,38 +111,38 @@ void initDisplay()
   waitForRedy();
   Serial.println("Reset");
 
-  //booster
-  sendCMD  (0x06);
-  sendData (0xc7); //11000111
-  sendData (0xcc); //11001100
-  sendData (0x28);
+  /**********************************release flash sleep**********************************/
+		sendCMD(0X65);			//FLASH CONTROL
+		sendCMD(0x01);
+
+		TFT_CS_LOW;					//MFCSB À­µÍ
+		MCU_write_flash(0xAB);
+		TFT_CS_HIGH;					//MFCSB À­¸ß
+
+		sendCMD(0X65);			//FLASH CONTROL
+		sendCMD(0x00);
+	/**********************************release flash sleep**********************************/
+
 
   //POWER SETTING
   sendCMD  (0x01);
   sendData (0x37); //110111
   sendData (0x00);
 
-  //POWER ON
-  sendCMD(0x04);
-  Serial.println("Power on");
-
-  waitForRedy();
-
   //PANNEL SETTING
   sendCMD  (0x00);
   sendData (0xCF); //11001111
   sendData (0x08); //1000
 
+  //booster
+  sendCMD  (0x06);
+  sendData (0xc7); //11000111
+  sendData (0xcc); //11001100
+  sendData (0x28);
+
   //PLL setting
   sendCMD  (0x30);
   sendData (0x3c);
-
-  //Resolution
-  sendCMD(0x61);
-  sendData (0x02);   //source 640
-  sendData (0x80);
-  sendData (0x01);   //gate 384
-  sendData (0x80);
 
   //TEMPERATURE SETTING
   sendCMD(0X41);
@@ -150,6 +156,13 @@ void initDisplay()
   sendCMD(0X60);
   sendData(0x22);
 
+  //Resolution
+  sendCMD(0x61);
+  sendData (0x02);   //source 640
+  sendData (0x80);
+  sendData (0x01);   //gate 384
+  sendData (0x80);
+
   //VDCS SETTING
   sendCMD(0X82);
   sendData(0x1E);    //decide by LUT file
@@ -160,41 +173,34 @@ void initDisplay()
 
   pic_display(0xFF);
 
+  //POWER ON
+  sendCMD(0x04);
+  Serial.println("Power on");
+  //waitForRedy();
+
   //display refresh
   sendCMD(0x12);
+  delay(100);
   waitForRedy();
+  /**********************************flash sleep**********************************/
+  		sendCMD(0X65);			//FLASH CONTROL
+  		sendCMD(0x01);
 
-  // sendCMD(0x02);
-  // waitForRedy();
+  		TFT_CS_LOW;					//MFCSB À­µÍ
+  		MCU_write_flash(0xB9);
+  		TFT_CS_HIGH;					//MFCSB À­¸ß
+
+  		sendCMD(0X65);			//FLASH CONTROL
+  		sendCMD(0x00);
+  /**********************************flash sleep**********************************/
+  sendCMD(0x02);
+  waitForRedy();
   //
-  // sendCMD(0x07);
-  // sendData(0xa5);
+  sendCMD(0x07);
+  sendData(0xa5);
 }
 
 unsigned char write = 1;
 void loop()
 {
-  SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
-  if(write == 1){
-    pic_display(0xFF);
-    Serial.println(0xFF);
-
-    write ++;
-  }else if(write == 2){
-    pic_display(0x00);
-    Serial.println(0x00);
-
-    write ++;
-  }
-  else if(write >= 3){
-    Serial.println(0x33);
-
-    pic_display(0x33);
-    write =1;
-  }
-  //display refresh
-  sendCMD(0x12);
-  waitForRedy();
-  SPI.endTransaction();
-
 }
